d1356cc5a6c712a111c2d2b0401bd1aeee7c9b3e
Kenneth J. Mighell <kenneth.j.mighell@nasa.gov>
2020-08-04 10:42:40 -0700

    modified check.py

--- mkpy3/check.py
+++ mkpy3/check.py
@@ -1,21 +1,52 @@
 #!/usr/bin/env python3
-import os
-import subprocess
-import glob
 
-verbose = False
+#file:///check.py
 
-cwd = os.getcwd()
-print(cwd, ' =$PWD')
-for name in glob.glob('./mkpy3*.py'):
-    result = subprocess.run(['python3', name], capture_output=True)
-    returncode = result.returncode
-    if (returncode != 0):
-        print(name, ': ***** ERROR FOUND *****')
+version_ = '2020AUG03T1030'
+
+# Kenneth John Mighell
+# Kepler Support Scientist
+# Kepler / K2 Science Office
+# NASA Ames Research Center / SETI Institute
+
+if (__name__ == '__main__'): 
+    """
+Purpose: Unit tests for mkpy3  (https://github.com/KenMighell/mkpy3)
+
+# Kenneth John Mighell
+# Kepler Support Scientist
+# Kepler / K2 Science Office
+# NASA Ames Research Center / SETI Institute
+    """
+    import os
+    import glob
+    import subprocess
+    #
+    cwd = os.getcwd()
+    print(cwd, ' =$PWD')
+    good = 0
+    bad = 0
+    print()
+    filel = sorted(glob.glob('./mkpy3*.py'))
+    for name in filel:
+        result = subprocess.run(['python3', name], capture_output=True)
+        returncode = result.returncode
+        if (returncode != 0):
+            bad += 1
+            print('\r',name, ': ***** ERROR FOUND *****\n')
+        else:
+            good += 1
+            print('\r',name,': OK',end='')
+            print('                                                        \r',
+              end='')
+        pass#if
+    pass#for
+    print()
+    if (good == len(filel)):
+        print('\nAll %d files PASS  :-)' % good)
     else:
-        if (verbose): print(name, ': OK')
+        print('%d of %d files FAIL  8=X' % (bad,good))
     pass#if
-pass#for
-
+    print()
+pass#id
 #EOF
-
