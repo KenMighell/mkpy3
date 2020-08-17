29fd77ad3c20d07052c2a569fd73a59bfa37135e
Kenneth J. Mighell <kenneth.j.mighell@nasa.gov>
2020-08-17 14:34:51 -0700

    update

--- mkpy3/mkpy3_finder_chart_image_show_v1.py
+++ mkpy3/mkpy3_finder_chart_image_show_v1.py
@@ -5,8 +5,12 @@
 # Kepler / K2 Science Office
 # NASA Ames Research Center / SETI Institute
 
+__version__ = '2020AUG17T1424 0.10'  # mkpy3_finder_chart_image_show_v1.py
 
-def mkpy3_finder_chart_image_show_v1(\
+# PEP8:OK
+
+
+def mkpy3_finder_chart_image_show_v1(
   ax=None,
   image_data=None,
   percentile=None,
@@ -17,8 +21,8 @@ def mkpy3_finder_chart_image_show_v1(\
     """
 Function: mkpy3_finder_chart_image_show()
 
-Purpose: 
-    
+Purpose:
+
 Plot the sky survey image data using matplotlib's ax.imshow.
 
 Parameters
@@ -39,40 +43,53 @@ verbose : bool (optional)
 Returns: nothing
 
 # Kenneth John Mighell
-# Kepler/K2 Support Scientist
-# Kepler/K2 Science Office
+# Kepler Support Scientist
+# Kepler / K2 Science Office
 # NASA Ames Research Center / SETI Institute
-    """    
-    from astropy.visualization import ImageNormalize, PercentileInterval, SqrtStretch
+    """
+    from astropy.visualization import ImageNormalize, PercentileInterval,\
+        SqrtStretch
     #
     assert(ax is not None)
     assert(image_data is not None)
-    if (percentile is None): percentile = 99
-    if (cmap is None): cmap = 'gray_r'
-    if (title is None): title = ''
-    if (verbose is None): verbose = False
+    if (percentile is None):
+        percentile = 99
+    # pass:if
+    if (cmap is None):
+        cmap = 'gray_r'
+    # pass:if
+    if (title is None):
+        title = ''
+    # pass:if
+    if (verbose is None):
+        verbose = False
+    # pass:if
     if (verbose):
-        print(percentile,'=percentile')
-        print("'%s' =cmap" % (cmap)) 
-        print(verbose,'=verbose')
+        print(percentile, '=percentile')
+        print("'%s' =cmap" % (cmap))
+        print(verbose, ' =verbose')
         print()
-    pass#if
+    # pass#if
     #
-    # ===== show the survey image with right ascension and declination coordinates =====
-    norm = ImageNormalize(image_data, interval=PercentileInterval(percentile), \
-      stretch=SqrtStretch())
-    ax.imshow(image_data,origin='lower',norm=norm,cmap=cmap)
+    # =========================================================================
+    # show the survey image with right ascension and declination coordinates
+    norm = ImageNormalize(
+        image_data, interval=PercentileInterval(percentile),
+        stretch=SqrtStretch())
+    ax.imshow(image_data, origin='lower', norm=norm, cmap=cmap)
     #
-    # set plot parameters    ax.tick_params(axis='y', labelsize=16 ) 
-    ax.tick_params(axis='x', labelsize=16, length=6, width=2) 
+    # set plot parameters
+    ax.tick_params(axis='x', labelsize=16, length=6, width=2)
     ax.tick_params(axis='y', labelsize=16, length=6, width=2)
-    ax.set_xlabel('Right Ascension (J2000)',size=24)
-    ax.set_ylabel('Declination (J2000)',size=24)
-    if (title != ''): ax.set_title(title,size=20)
+    ax.set_xlabel('Right Ascension (J2000)', size=24)
+    ax.set_ylabel('Declination (J2000)', size=24)
+    if (title != ''):
+        ax.set_title(title, size=20)
+    # pass:if
     ax.set_aspect(1)
     #
     return
-pass#def
+# pass:def
 
 
 if (__name__ == '__main__'):
@@ -82,52 +99,54 @@ if (__name__ == '__main__'):
     import astropy.units as u
     import os
     import ntpath
-    
+
     import mkpy3_finder_chart_survey_fits_image_get_v1 as km1
-    
+
     # Exoplanet Kelper-138b is "KIC 7603200":
-    tpf = lk.search_targetpixelfile(target='kepler-138b',mission='kepler',\
-      quarter=10).download(quality_bitmask=0)  
+    tpf = lk.search_targetpixelfile(
+        target='kepler-138b', mission='kepler',
+        quarter=10).download(quality_bitmask=0)
     print('TPF filename:', ntpath.basename(tpf.path))
     print('TPF dirname: ', os.path.dirname(tpf.path))
-    
+
     target = 'Kepler-138b'
     title_ = tpf.hdu[0].header['object']
     title_ += ' : Exoplanet '+target
 
     ra_deg = tpf.ra
     dec_deg = tpf.dec
-    
+
     # get survey image data
     width_height_arcmin = 3.00
     survey = '2MASS-J'
     survey_hdu, survey_hdr, survey_data, survey_wcs, survey_cframe = \
-      km1.mkpy3_finder_chart_survey_fits_image_get_v1(ra_deg, dec_deg, 
-      radius_arcmin=width_height_arcmin, survey=survey, verbose=True)
-    
+        km1.mkpy3_finder_chart_survey_fits_image_get_v1(
+            ra_deg, dec_deg, radius_arcmin=width_height_arcmin, survey=survey,
+            verbose=True)
+
     # create a matplotlib figure object
-    fig = plt.figure(figsize=(12,12));
-    
+    fig = plt.figure(figsize=(12, 12))
+
     # create a matplotlib axis object with right ascension and declination axes
     ax = plt.subplot(projection=survey_wcs)
-            
+
     # show the survey image
     percentile = 99.0
-    mkpy3_finder_chart_image_show_v1(ax=ax, image_data=survey_data, \
-      percentile=percentile, verbose=True)
-    
+    mkpy3_finder_chart_image_show_v1(
+        ax=ax, image_data=survey_data, percentile=percentile, verbose=True)
+
     # replace title with a custom title
-    ax.set_title(title_,size=24)
-    
+    ax.set_title(title_, size=24)
+
     # put a yellow circle at the target position
-    ax.scatter(ra_deg*u.deg, dec_deg*u.deg, \
-      transform=ax.get_transform(survey_cframe), \
-      s=600, edgecolor='yellow', facecolor='None', lw=3, zorder=10);
-    
+    ax.scatter(
+        ra_deg*u.deg, dec_deg*u.deg, transform=ax.get_transform(survey_cframe),
+        s=600, edgecolor='yellow', facecolor='None', lw=3, zorder=10)
+
     pname = 'mkpy3_plot.png'
-    if (pname != ''): 
-        plt.savefig(pname, bbox_inches = "tight")
-        print(pname,' <--- plot filename has been written!  :-)\n')
-    pass#if
-pass#if
-#EOF
+    if (pname != ''):
+        plt.savefig(pname, bbox_inches="tight")
+        print(pname, ' <--- plot filename has been written!  :-)\n')
+    # pass:if
+# pass:if
+# EOF
