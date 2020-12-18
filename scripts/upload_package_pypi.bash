bf70f398d35081241877a4b4772915bac5158e93
Ken Mighell <kmighell@mmoc5037.local>
2020-12-18 12:11:54 -0800

    update

---
+++ scripts/upload_package_pypi.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+python3 -m twine upload dist/*
+#
+# example:
+#
+#$ python3 -m twine upload dist/*
+#Uploading distributions to https://upload.pypi.org/legacy/
+#Enter your username: KenMighell
+#Enter your password: 
+#Uploading mkpy3-1.0.1b1-py3-none-any.whl
+#100%|██████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████| 50.7k/50.7k [00:02<00:00, 23.8kB/s]
+#Uploading mkpy3-1.0.1b1.tar.gz
+#100%|█████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████| 337k/337k [00:01<00:00, 225kB/s]
+#
+#View at:
+#https://pypi.org/project/mkpy3/1.0.1b1/
+#
+#EOF
+
